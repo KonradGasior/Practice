9ae52d8b04661a1170571c544e9c211ebc77135b
Nebiron <42714837+Nebiron@users.noreply.github.com>
2018-08-26 13:46:58 +0100

    Add files via upload

---
+++ Room_main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Room_var.h"
+#include <string>
+int main()
+{
+	Room room_1;
+	float wid, len, h;
+	std::string uni;
+
+	get_unit(uni);
+
+	room_1.get_dimensions(wid, len, h);
+	room_1.calc_area(wid, len);
+	room_1.volume(h);
+	room_1.calc_paint(len, wid, h, uni);
+	room_1.show(room_1, uni);
+
+	system("PAUSE");
+	return 0;
+}
